f9c8ade8ff0625835211342e2f629c760e513448
Artur Novikov <artur.20177102@mail.ru>
2025-05-27 21:20:13 +0300

    completed desktop header and footer

--- frontend/b821v/src/components/common/Footer.vue
+++ frontend/b821v/src/components/common/Footer.vue
@@ -1,1 +1,70 @@
-<template>снизу</template>
+<template>
+  <div class="footer main-wrapper">
+    <hr class="main-hr" />
+    <div class="footer__container">
+      <UiIcon name="logo-white" class="footer__logo" />
+      <ul class="footer__list">
+        <li class="footer__list-item">
+          <UiIcon name="phone" class="footer__item-icon" />
+          <div class="footer__item-label">Многоканальный телефон:</div>
+          <div>+7 (777) 777-77-77</div>
+        </li>
+        <li class="footer__list-item">
+          <UiIcon name="mail" class="footer__item-icon" />
+          <div class="footer__item-label">E-mail:</div>
+          <div>artur.20166102@mail.ru</div>
+        </li>
+        <li class="footer__list-item">
+          <UiIcon name="profile2" class="footer__item-icon" />
+          <div class="footer__item-label">Кабинет клиента:</div>
+          <div class="footer__item-link">войти в личный кабинет</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import UiIcon from "../ui/UiIcon.vue";
+
+export default {
+  name: "Footer",
+  components: {
+    UiIcon,
+  },
+};
+</script>
+
+<style lang="stylus">
+.footer
+    background-color:$secondMainColor
+    &__container
+        height: 160px
+        background-color:$secondMainColor
+        color:$thirdMainColor
+        display flex
+        align-items: center
+        justify-content space-between
+    &__logo
+        height 41px
+    &__profile
+        height 49px
+    &__list
+        display flex
+        align-items center
+        justify-content space-between
+        gap 6vw
+    &__list-item
+        font-size 16px
+        text-transform uppercase
+        text-decoration none;
+        list-style-type none;
+        display flex
+        flex-direction column
+        gap 10px
+    &__item-icon
+        height 30px
+        align-self flex-start
+    &__item-link
+        line-height 25px
+</style>
--- frontend/b821v/src/components/common/Header.vue
+++ frontend/b821v/src/components/common/Header.vue
@@ -1,8 +1,16 @@
 <template class="header">
-  <div class="header__container main-wrapper">
-    <UiIcon name="logo-white" />
-    <div class="header__list"></div>
-    <img class="header__profile" src="" alt="" />
+  <div>
+    <div class="header__container main-wrapper">
+      <UiIcon name="logo-white" class="header__logo" />
+      <ul class="header__list">
+        <li class="header__list-item">Новости</li>
+        <li class="header__list-item">Аукционы</li>
+        <li class="header__list-item">Оформление заказа</li>
+        <li class="header__list-item">Профиль</li>
+      </ul>
+      <UiIcon name="profile" class="header__profile" />
+    </div>
+    <hr class="main-hr" />
   </div>
 </template>
 
@@ -23,4 +31,21 @@ export default {
         height: 70px
         background-color:$secondMainColor
         color:$thirdMainColor
+        display flex
+        align-items: center
+        justify-content space-between
+    &__logo
+        height 41px
+    &__profile
+        height 49px
+    &__list
+        display flex
+        align-items center
+        justify-content space-between
+        gap 6vw
+    &__list-item
+      font-size 16px
+      text-transform uppercase
+      text-decoration none;
+      list-style-type none;
 </style>
