e252d69c2910e2f23200c34f8412841f67c1d841
Artur Novikov <artur.20177102@mail.ru>
2025-06-07 22:06:30 +0300

    completed text img in  AuctionsCategories

--- frontend/b821v/src/components/AuctionsCategory.vue
+++ frontend/b821v/src/components/AuctionsCategory.vue
@@ -1,61 +1,84 @@
 <template>
   <div class="auctions-category">
     <div class="auctions-category__string">
-      <img
-        src="/src/assets/img/auctionGallary/gallary1cut.png"
-        alt="Left trapezoid"
-        class="auctions-category__layer auctions-category__one-left"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary2cut.png"
-        alt="Center parallelogram"
-        class="auctions-category__layer auctions-category__one-center"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary3cut.png"
-        alt="Right trapezoid"
-        class="auctions-category__layer auctions-category__one-right"
-      />
+      <div class="image-wrapper auctions-category__one-left">
+        <img
+          src="/src/assets/img/auctionGallary/gallary1cut.png"
+          alt="Left trapezoid"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">спорт</div>
+      </div>
+      <div class="image-wrapper auctions-category__one-center">
+        <img
+          src="/src/assets/img/auctionGallary/gallary2cut.png"
+          alt="Center parallelogram"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">премиум</div>
+      </div>
+      <div class="image-wrapper auctions-category__one-right">
+        <img
+          src="/src/assets/img/auctionGallary/gallary3cut.png"
+          alt="Right trapezoid"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">немцы</div>
+      </div>
     </div>
+
     <div class="auctions-category__string">
-      <img
-        src="/src/assets/img/auctionGallary/gallary4cut.png"
-        alt="Left trapezoid"
-        class="auctions-category__layer auctions-category__two-left"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary5cut.png"
-        alt="Center parallelogram"
-        class="auctions-category__layer auctions-category__two-center"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary6cut.png"
-        alt="Right trapezoid"
-        class="auctions-category__layer auctions-category__two-right"
-      />
+      <div class="image-wrapper auctions-category__two-left">
+        <img
+          src="/src/assets/img/auctionGallary/gallary4cut.png"
+          alt="Left trapezoid"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">внедорожники</div>
+      </div>
+      <div class="image-wrapper auctions-category__two-center">
+        <img
+          src="/src/assets/img/auctionGallary/gallary5cut.png"
+          alt="Center parallelogram"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">для семьи</div>
+      </div>
+      <div class="image-wrapper auctions-category__two-right">
+        <img
+          src="/src/assets/img/auctionGallary/gallary6cut.png"
+          alt="Right trapezoid"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">китайцы</div>
+      </div>
     </div>
+
     <div class="auctions-category__string">
-      <img
-        src="/src/assets/img/auctionGallary/gallary7cut.png"
-        alt="Left trapezoid"
-        class="auctions-category__layer auctions-category__three-left"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary8cut.png"
-        alt="Center parallelogram"
-        class="auctions-category__layer auctions-category__three-center"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary9cut.png"
-        alt="Right trapezoid"
-        class="auctions-category__layer auctions-category__three-right"
-      />
+      <div class="image-wrapper auctions-category__three-left">
+        <img
+          src="/src/assets/img/auctionGallary/gallary7cut.png"
+          alt="Left trapezoid"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">японцы</div>
+      </div>
+      <div class="image-wrapper auctions-category__three-center">
+        <img
+          src="/src/assets/img/auctionGallary/gallary8cut.png"
+          alt="Center parallelogram"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">русские</div>
+      </div>
+      <div class="image-wrapper auctions-category__three-right">
+        <img
+          src="/src/assets/img/auctionGallary/gallary9cut.png"
+          alt="Right trapezoid"
+          class="auctions-category__layer"
+        />
+        <div class="image-text">кары</div>
+      </div>
     </div>
   </div>
 </template>
@@ -64,54 +87,90 @@
 
 <style lang="stylus" scoped>
 .auctions-category
-    padding 40px 0
-    background-color $secondMainColor
-    &__string
-        position relative
-        height 160px
-    &__layer
-        position absolute
-        width 20vw
-        height 150px
-    &__one-left
-        left 80px
-        top 0
-        width 30vw
-    &__one-center
-        width 40vw
-        left 24.5vw
-        top 0
-    &__one-right
-        width 40.65vw
-        right 80px
-        top 0
-     &__two-left
-        left 80px
-        top 0
-        width 43vw
-    &__two-center
-        width 39vw
-        left 37.5vw
-        top 0
-    &__two-right
-        width 28.65vw
-        right 80px
-        top 0
-     &__three-left
-        left 80px
-        top 0
-        width 43vw
-    &__three-center
-        width 39vw
-        left 37.5vw
-        top 0
-    &__three-right
-        width 28.65vw
-        right 80px
-        top 0
-.auctions-category__layer
-  transition filter 0.3s ease
+  padding 40px 0
+  background-color $secondMainColor
 
-.auctions-category__layer:hover
-  filter brightness(50%)
+  &__string
+    position relative
+    height 160px
+
+  &__layer
+    width 100%
+    height 100%
+    display block
+    transition filter 0.3s ease
+
+  &__layer:hover
+    filter brightness(50%)
+
+.image-wrapper
+  position absolute
+  height 150px
+  width 20vw
+  overflow hidden
+
+  &:hover .image-text
+    opacity 1
+
+.image-text
+  position absolute
+  top 50%
+  left 50%
+  transform translate(-50%, -50%)
+  color white
+  font-size 40px
+  font-weight 700
+
+  text-align center
+  text-transform uppercase
+  pointer-events none
+  opacity 0
+  transition opacity 0.3s ease
+
+// Позиции для каждого блока
+.auctions-category
+  &__one-left
+    left 80px
+    top 0
+    width 30vw
+
+  &__one-center
+    left 24.5vw
+    top 0
+    width 40vw
+
+  &__one-right
+    right 80px
+    top 0
+    width 40.65vw
+
+  &__two-left
+    left 80px
+    top 0
+    width 43vw
+
+  &__two-center
+    left 37.5vw
+    top 0
+    width 39vw
+
+  &__two-right
+    right 80px
+    top 0
+    width 28.65vw
+
+  &__three-left
+    left 80px
+    top 0
+    width 43vw
+
+  &__three-center
+    left 37.5vw
+    top 0
+    width 39vw
+
+  &__three-right
+    right 80px
+    top 0
+    width 28.65vw
 </style>
--- frontend/b821v/src/views/AuctionsCategory.vue
+++ frontend/b821v/src/views/AuctionsCategory.vue
@@ -1,67 +1,84 @@
 <template>
   <div class="auctions-category">
     <div class="first-string">
-      <!-- Левый «трапеция»-кусочек -->
-      <img
-        src="/src/assets/img/auctionGallary/gallary1cut.png"
-        alt="Left trapezoid"
-        class="first-string__layer first-string__one-left"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary2cut.png"
-        alt="Center parallelogram"
-        class="first-string__layer first-string__one-center"
-      />
-
-      <!-- Правый «трапеция»-кусочек (зеркально) -->
-      <img
-        src="/src/assets/img/auctionGallary/gallary3cut.png"
-        alt="Right trapezoid"
-        class="first-string__layer first-string__one-right"
-      />
+      <div class="image-wrapper first-string__one-left">
+        <img
+          src="/src/assets/img/auctionGallary/gallary1cut.png"
+          alt="Left trapezoid"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 1</div>
+      </div>
+      <div class="image-wrapper first-string__one-center">
+        <img
+          src="/src/assets/img/auctionGallary/gallary2cut.png"
+          alt="Center parallelogram"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 2</div>
+      </div>
+      <div class="image-wrapper first-string__one-right">
+        <img
+          src="/src/assets/img/auctionGallary/gallary3cut.png"
+          alt="Right trapezoid"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 3</div>
+      </div>
     </div>
+
     <div class="first-string">
-      <!-- Левый «трапеция»-кусочек -->
-      <img
-        src="/src/assets/img/auctionGallary/gallary4cut.png"
-        alt="Left trapezoid"
-        class="first-string__layer first-string__two-left"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary5cut.png"
-        alt="Center parallelogram"
-        class="first-string__layer first-string__two-center"
-      />
-
-      <!-- Правый «трапеция»-кусочек (зеркально) -->
-      <img
-        src="/src/assets/img/auctionGallary/gallary6cut.png"
-        alt="Right trapezoid"
-        class="first-string__layer first-string__two-right"
-      />
+      <div class="image-wrapper first-string__two-left">
+        <img
+          src="/src/assets/img/auctionGallary/gallary4cut.png"
+          alt="Left trapezoid"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 4</div>
+      </div>
+      <div class="image-wrapper first-string__two-center">
+        <img
+          src="/src/assets/img/auctionGallary/gallary5cut.png"
+          alt="Center parallelogram"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 5</div>
+      </div>
+      <div class="image-wrapper first-string__two-right">
+        <img
+          src="/src/assets/img/auctionGallary/gallary6cut.png"
+          alt="Right trapezoid"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 6</div>
+      </div>
     </div>
+
     <div class="first-string">
-      <!-- Левый «трапеция»-кусочек -->
-      <img
-        src="/src/assets/img/auctionGallary/gallary7cut.png"
-        alt="Left trapezoid"
-        class="first-string__layer first-string__three-left"
-      />
-
-      <img
-        src="/src/assets/img/auctionGallary/gallary8cut.png"
-        alt="Center parallelogram"
-        class="first-string__layer first-string__three-center"
-      />
-
-      <!-- Правый «трапеция»-кусочек (зеркально) -->
-      <img
-        src="/src/assets/img/auctionGallary/gallary9cut.png"
-        alt="Right trapezoid"
-        class="first-string__layer first-string__three-right"
-      />
+      <div class="image-wrapper first-string__three-left">
+        <img
+          src="/src/assets/img/auctionGallary/gallary7cut.png"
+          alt="Left trapezoid"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 7</div>
+      </div>
+      <div class="image-wrapper first-string__three-center">
+        <img
+          src="/src/assets/img/auctionGallary/gallary8cut.png"
+          alt="Center parallelogram"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 8</div>
+      </div>
+      <div class="image-wrapper first-string__three-right">
+        <img
+          src="/src/assets/img/auctionGallary/gallary9cut.png"
+          alt="Right trapezoid"
+          class="first-string__layer"
+        />
+        <div class="image-text">Текст 9</div>
+      </div>
     </div>
   </div>
 </template>
@@ -95,52 +112,80 @@ const props = defineProps({
 .auctions-category
   padding 40px 0
   background-color $secondMainColor
-.first-string
-    position relative
-    height 160px
-    &__layer
-        position absolute
-        width 20vw
-        height 150px
-    &__one-left
-        left 80px
-        top 0
-        width 30vw
-    &__one-center
-        width 40vw
-        left 24.5vw
-        top 0
-    &__one-right
-        width 40.65vw
-        right 80px
-        top 0
-     &__two-left
-        left 80px
-        top 0
-        width 43vw
-    &__two-center
-        width 39vw
-        left 37.5vw
-        top 0
-    &__two-right
-        width 28.65vw
-        right 80px
-        top 0
-     &__three-left
-        left 80px
-        top 0
-        width 43vw
-    &__three-center
-        width 39vw
-        left 37.5vw
-        top 0
-    &__three-right
-        width 28.65vw
-        right 80px
-        top 0
-.first-string__layer
-  transition filter 0.3s ease // Плавность анимации
 
-.first-string__layer:hover
-  filter brightness(50%) // Затемнение
+.first-string
+  position relative
+  height 160px
+
+.image-wrapper
+  position absolute
+  display inline-block
+  height 150px
+  width 20vw
+
+  img
+    width 100%
+    height 100%
+    display block
+    transition filter 0.3s ease
+
+  &:hover img
+    filter brightness(50%)
+
+.image-text
+  position absolute
+  top 50%
+  left 50%
+  transform translate(-50%, -50%)
+  color white
+  font-size 24px
+  font-weight bold
+  text-align center
+  pointer-events none
+
+// Позиции изображений
+.first-string__one-left
+  left 80px
+  width 30vw
+  top 0
+
+.first-string__one-center
+  left 24.5vw
+  width 40vw
+  top 0
+
+.first-string__one-right
+  right 80px
+  width 40.65vw
+  top 0
+
+.first-string__two-left
+  left 80px
+  width 43vw
+  top 0
+
+.first-string__two-center
+  left 37.5vw
+  width 39vw
+  top 0
+
+.first-string__two-right
+  right 80px
+  width 28.65vw
+  top 0
+
+.first-string__three-left
+  left 80px
+  width 43vw
+  top 0
+
+.first-string__three-center
+  left 37.5vw
+  width 39vw
+  top 0
+
+.first-string__three-right
+  right 80px
+  width 28.65vw
+  top 0
 </style>
