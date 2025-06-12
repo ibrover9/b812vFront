b72893f38905c1edfa205a124ca71623c80a17d2
Artur Novikov <artur.20177102@mail.ru>
2025-06-12 14:38:05 +0300

    add new component with page Profile

---
+++ frontend/b821v/src/components/profile/ProfileContract.vue
@@ -0,0 +1,1 @@
+<template><h1>Скачать Договора</h1></template>
--- frontend/b821v/src/views/Profile.vue
+++ frontend/b821v/src/views/Profile.vue
@@ -2,13 +2,16 @@
   <div class="profile">
     <UiHeaderPage :items="headerItems" />
     <ProfileData />
+    <ProfileHistoryAuction />
+    <ProfilwContract />
   </div>
 </template>
 
 <script setup>
 import ProfileData from "../components/profile/ProfileData.vue";
 import UiHeaderPage from "../components/ui/UiHeaderPage.vue";
-
+import ProfileHistoryAuction from "../components/profile/ProfileHistoryAuction.vue";
+import ProfilwContract from "../components/profile/ProfileContract.vue";
 const headerItems = [
   { label: "История ставок", targetId: "historyBid" },
   { label: "Скачать договоры", targetId: "contracts" },
